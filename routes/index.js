2ca595df9a1fb28748e726cb60fe7f4461077e4a
Jane Doe <janedoe@example.com>
2020-07-03 15:32:15 +1200

    created pages

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
