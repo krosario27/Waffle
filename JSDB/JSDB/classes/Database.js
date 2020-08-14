cb846213cb12110063752ac0aaa792a7bf38c362
Jane Doe <janedoe@example.com>
2020-08-14 15:56:46 +1200

    added error messages

---
+++ JSDB/JSDB/classes/Database.js
@@ -0,0 +1,50 @@
+class Database{
+
+    constructor(){
+        
+        if (typeof this.instance === 'object') {
+            return this.instance;
+        }
+        this.fs = require('fs');
+        this.db = './db.json';
+
+        let rawdata = this.fs.readFileSync(this.db);
+        this.users = JSON.parse(rawdata);
+        this.instance = this;
+        return this;
+    }
+
+    login(email, password) {
+        var login = false
+        this.users.forEach(function(user){
+            if (user.hasOwnProperty(email) && user[email] === password) {
+                login = true;
+            }
+        });
+        return login;
+    }
+
+    register(email, password) {
+        let newUser = {};
+        newUser[email] = password;
+        this.users.push(newUser);
+        this.fs.writeFileSync(this.db, JSON.stringify(this.users));
+        return true;
+    }
+
+    userExists(email){
+        var userExists = false
+        this.users.forEach(function(user){
+            if (user.hasOwnProperty(email)) {
+                userExists = true;
+            }
+        });
+        return userExists;
+    }
+
+    stall = async (stallTime = 3000) => {
+        await new Promise(resolve => setTimeout(resolve, stallTime));
+    }
+
+}
+module.exports = Database;
